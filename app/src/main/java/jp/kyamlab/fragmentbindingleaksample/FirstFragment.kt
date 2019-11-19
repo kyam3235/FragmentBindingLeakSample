d699354c91fa5e93038976082dc6db168046bacd
Kyohei Yamaguchi <kyohei3235@gmail.com>
2019-11-20 00:29:47 +0900

    initial commit

---
+++ app/src/main/java/jp/kyamlab/fragmentbindingleaksample/FirstFragment.kt
@@ -0,0 +1,41 @@
+package jp.kyamlab.fragmentbindingleaksample
+
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import jp.kyamlab.fragmentbindingleaksample.databinding.FragmentFirstBinding
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class FirstFragment : Fragment() {
+
+    private lateinit var binding: FragmentFirstBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding =
+            DataBindingUtil.inflate(
+                inflater,
+                R.layout.fragment_first,
+                container,
+                false
+            )
+        binding.lifecycleOwner = viewLifecycleOwner
+
+        binding.nextButton.setOnClickListener {
+            requireActivity().supportFragmentManager.beginTransaction()
+                .replace(R.id.container, SecondFragment())
+                .addToBackStack(null)
+                .commit()
+        }
+
+        return binding.root
+    }
+}
---
+++ app/src/main/java/jp/kyamlab/fragmentbindingleaksample/SecondFragment.kt
@@ -0,0 +1,37 @@
+package jp.kyamlab.fragmentbindingleaksample
+
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import jp.kyamlab.fragmentbindingleaksample.databinding.FragmentSecondBinding
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class SecondFragment : Fragment() {
+
+    private lateinit var binding: FragmentSecondBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = DataBindingUtil.inflate(
+            inflater,
+            R.layout.fragment_second,
+            container,
+            false
+        )
+        binding.lifecycleOwner = viewLifecycleOwner
+
+        binding.backButton.setOnClickListener {
+            requireActivity().supportFragmentManager.popBackStack()
+        }
+
+        return binding.root
+    }
+}
